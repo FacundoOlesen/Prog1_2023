38baf518122976d10f9e1b9569e17c5d33fc28a9
Facundo Olesen <facu.olesen14@gmail.com>
2023-04-19 20:07:50 -0300

    Agrego ej ordenamiento

---
+++ src/arreglos/ej1.java
@@ -0,0 +1,33 @@
+package arreglos;
+
+import java.util.Random;
+
+public class ej1 {
+    public static void main(String[]args) {
+        int [] arrentero;
+        int aux;
+        arrentero = new int [8];
+        cargar_arreglo_aleatorio_int(arrentero);
+        for(int recorredor=0; recorredor<=7;recorredor++){
+            for(int cursor=recorredor;cursor<8;cursor++){
+                if(arrentero[recorredor]<arrentero[cursor]){
+                    aux=arrentero[recorredor];
+                    arrentero[recorredor]=arrentero[cursor];
+                    arrentero[cursor]=aux;
+                }
+            }
+        }
+        imprimir_arreglo_int(arrentero);
+    }
+    public static void cargar_arreglo_aleatorio_int(int [] arr){
+        Random r = new Random();
+        for (int pos = 0; pos < 8; pos++){
+        arr[pos]=(r.nextInt(9-0+1) + 0);
+        }
+    }
+    public static void imprimir_arreglo_int(int [] arr){
+        for (int pos = 0; pos < 8; pos++){
+        System.out.println("nombre_arreglo["+pos+"]=>: "+arr[pos]);
+        }
+    }
+}
