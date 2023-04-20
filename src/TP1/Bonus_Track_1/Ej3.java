9fef7f20ce8890a0769ec7debd24d5b6e55842ef
Facundo Olesen <faolesen@alumnos.exa.unicen.edu.ar>
2023-04-20 20:31:23 -0300

    Termino B-T TP1.

--- src/TP1/Bonus_Track_1/Ej3.java
+++ src/TP1/Bonus_Track_1/Ej3.java
@@ -2,7 +2,7 @@ package TP1.Bonus_Track_1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-public class Ej2 {
+public class Ej3 {
     public static void main(String[] args) {
         char tipofactura;
         int numero;
@@ -15,12 +15,13 @@ public class Ej2 {
         int dia;
         double porcentaje_descuento;
         double descuento; 
+        int dni;
     
        
         try {
             BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.println ("Ingrese el dia de la compra (1=Martes, 2=Jueves, Numeros restantes= cualq. otro dia): ");
+            System.out.println ("Ingrese el dia de la compra (1=Viernes, 2=Sabado, Numeros restantes= cualq. otro dia): ");
             dia = Integer.valueOf(entrada.readLine());
 
             System.out.println ("Ingrese el tipo de factura: ");
@@ -31,6 +32,9 @@ public class Ej2 {
 
             System.out.println ("Ingrese su nombre: ");
             nombre = entrada.readLine();
+
+            System.out.println ("Ingrese su DNI: ");
+            dni = Integer.valueOf(entrada.readLine());
 
             System.out.println ("Ingrese el producto 1: ");
             producto1 = entrada.readLine();
@@ -46,16 +50,19 @@ public class Ej2 {
 
             importetotal= importe1+importe2;
 
+            System.out.println("Dia:  \t" + dia);
 
             System.out.println("Factura " + tipofactura + "\tN " + numero);
 
             System.out.println("Nombre:  \t" + nombre);
+            System.out.println("DNI:  \t" + dni);
+
 
             System.out.println("Producto   \t  Importe" );
 
 
-            if(dia==1&&importetotal>13000){
-                porcentaje_descuento=5;
+            if((dia==1)||(dia==2) &&importetotal>15000 && (numero%2) ==0 && (dni%2)!=0){
+                porcentaje_descuento=8;
                 descuento=((importe1+importe2)*porcentaje_descuento)/100;
                 System.out.println(producto1 + "   \t" + importe1);
                 System.out.println(producto2 + "   \t" + importe2);
@@ -63,32 +70,7 @@ public class Ej2 {
                 System.out.println("Precio final de: " + "\t" + ((importe1+importe2)-descuento));
             }
 
-            else if(dia==1&&importetotal>20000){
-                porcentaje_descuento=7.5;
-                descuento=((importe1+importe2)*porcentaje_descuento)/100;
-                System.out.println(producto1 + "   \t" + importe1);
-                System.out.println(producto2 + "   \t" + importe2);
-                System.out.println("El descuento es de: " + "\t" +  descuento);
-                System.out.println("Precio final de: " + "\t" + ((importe1+importe2)-descuento));
-            }
-
-            else if(dia==2&&importetotal>25000){
-                porcentaje_descuento=10;
-                int tope_reintegro = 3000;
-                System.out.println(producto1 + "    \t" + importe1);
-                System.out.println(producto2 + "       \t" + importe2); 
-                descuento=((importe1+importe2)*porcentaje_descuento)/100;
-                if(descuento<=tope_reintegro){
-                    System.out.println("El descuento es de: " + "\t" +  descuento);
-                    System.out.println("Precio final de: " + "\t" + ((importe1+importe2)-descuento));
-                }
-                else if(descuento>tope_reintegro)  {
-                    System.out.println("El descuento es de: " + "\t" +  tope_reintegro);
-                    System.out.println("Precio final de: " + "\t" + ((importe1+importe2)-tope_reintegro));
-                }
-            }
-
-            else {
+            else  {
                 importetotal=importe1+importe2;
                
                 System.out.println(producto1 +  "     \t" + importe1);
@@ -96,6 +78,7 @@ public class Ej2 {
                 System.out.println("   \t  Importe total" );
 
                 System.out.println("   \t   " + importetotal);
+                System.out.println("No hay descuentos! :(");
             }
 
 
--- src/arreglos/ej1.java
+++ src/arreglos/ej1.java
@@ -8,9 +8,12 @@ public class ej1 {
         int aux;
         arrentero = new int [8];
         cargar_arreglo_aleatorio_int(arrentero);
-        for(int recorredor=0; recorredor<=7;recorredor++){
+        for(int recorredor=0; recorredor<8;recorredor++){
+
             for(int cursor=recorredor;cursor<8;cursor++){
+
                 if(arrentero[recorredor]<arrentero[cursor]){
+                    
                     aux=arrentero[recorredor];
                     arrentero[recorredor]=arrentero[cursor];
                     arrentero[cursor]=aux;
@@ -19,12 +22,14 @@ public class ej1 {
         }
         imprimir_arreglo_int(arrentero);
     }
+
     public static void cargar_arreglo_aleatorio_int(int [] arr){
         Random r = new Random();
         for (int pos = 0; pos < 8; pos++){
         arr[pos]=(r.nextInt(9-0+1) + 0);
         }
     }
+    
     public static void imprimir_arreglo_int(int [] arr){
         for (int pos = 0; pos < 8; pos++){
         System.out.println("nombre_arreglo["+pos+"]=>: "+arr[pos]);
