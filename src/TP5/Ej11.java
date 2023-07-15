823a695b98132ea1b58f116fa788c5ea03e5d6e2
Facundo Olesen <faolesen@alumnos.exa.unicen.edu.ar>
2023-07-15 14:33:02 -0300

    Arreglo EJ11

--- src/TP5/Ej11.java
+++ src/TP5/Ej11.java
@@ -1,23 +1,25 @@
 package TP5;
+
 import java.util.Random;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 public class Ej11 {
     public static int MAX = 10;
     public static final int MAXVALOR = 10;
     public static final int MINVALOR = 1;
     public static final int MIN = 0;
+
     public static void main(String[] args) {
-        
+
         int arr[] = new int[MAX];
         cargar_arreglo_aleatorio_int(arr);
         imprimirArreglo(arr);
-        int numero= obtenerNumeroAEliminar();
-        
-            System.out.println("Arreglo con: " + numero + " eliminado: ");
-            eliminar_ocurrencias(arr, numero);
-            imprimirArreglo(arr);
-        
+        int numero = obtenerNumeroAEliminar();
+        System.out.println("Arreglo con: " + numero + " eliminado: ");
+        eliminar_ocurrencias(arr, numero);
+        imprimirArreglo(arr);
+
     }
 
     public static void cargar_arreglo_aleatorio_int(int[] arr) {
@@ -33,12 +35,13 @@ public class Ej11 {
         }
     }
 
-    public static int obtenerNumeroAEliminar(){
+    public static int obtenerNumeroAEliminar() {
         int numero = 0;
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
         try {
-            System.out.println("Ingrese el numero que quiere eliminar del arreglo. (Se eliminaran todas sus existencias si existe)");
-            numero= Integer.valueOf(entrada.readLine());
+            System.out.println(
+                    "Ingrese el numero que quiere eliminar del arreglo. (Se eliminaran todas sus existencias si existe)");
+            numero = Integer.valueOf(entrada.readLine());
         }
 
         catch (Exception e) {
@@ -47,17 +50,20 @@ public class Ej11 {
         return numero;
     }
 
-    public static void eliminar_ocurrencias(int[]arr, int numero){
-        for (int i=0; i<MAX; i++){
-            if(arr[i]==numero){
-               corrimiento_izquierda(arr, i);
+    public static void eliminar_ocurrencias(int[] arr, int numero) {
+        if (arr[MAX - 1] == numero) {
+            arr[MAX - 1] = -1;
+        }
+        for (int i = 0; i < MAX; i++) {
+            if (arr[i] == numero) {
+                corrimiento_izquierda(arr, i);
             }
         }
     }
 
-    public static void corrimiento_izquierda(int[]arr, int pos){
-        while(pos<MAX-1){
-            arr[pos]=arr[pos+1];
+    public static void corrimiento_izquierda(int[] arr, int pos) {
+        while (pos < MAX - 1) {
+            arr[pos] = arr[pos + 1];
             pos++;
         }
     }
